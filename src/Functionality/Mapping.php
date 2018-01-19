7c0cef94bf0acfbbd8d1e2c926c9a602c857a55c
HDVinnie <unit3d@protonmail.com>
2018-01-19 09:04:43 -0500

    Update Mapping.php
    - added missing torrent fields

--- src/Functionality/Mapping.php
+++ src/Functionality/Mapping.php
@@ -46,13 +46,24 @@ class Mapping
     public static function mapTorrent(\stdClass $data): array
     {
         return [
-            'info_hash'   => $data->info_hash,
             'name'        => $data->name ?? null,
+            'info_hash'   => $data->info_hash,
             'size'        => $data->size ?? 0,
             'description' => $data->descr ?? null,
             'seeders'     => $data->seeders ?? 0,
             'leechers'    => $data->leechers ?? 0,
+            'times_completed'    => $data->times_completed ?? 0,
             'num_files'   => $data->numfiles ?? 0,
+            'file_name'   => $data->filename,
+            'nfo'   => $data->nfo,
+            'sticky'   => $data->sticky,
+            'free'   => $data->freetorrent,
+            'user_id'   => $data->owner,
+            'anon'   => $data->anonymous,
+            'status'   => '1',
+            'type'   => $data->type,
+            'category_id'   => '1',
+            'anon'   => $data->anonymous,
             'created_at'  => Carbon::createFromTimestamp(strtotime($data->added)),
         ];
     }
